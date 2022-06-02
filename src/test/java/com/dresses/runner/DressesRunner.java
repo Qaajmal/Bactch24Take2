a0e9cb06f76dba917c0246df36c835dc51464497
ajmal <qaajmal@gmail.com>
2022-06-02 19:54:25 -0400

    push 7

---
+++ src/test/java/com/dresses/runner/DressesRunner.java
@@ -0,0 +1,21 @@
+package com.dresses.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith (Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources", // THE LOCATION TO SAVE THE FEATURE FILE
+		glue= {"com.dresses.steps"}, // PAKAGE NAME OF YOUR STEP DEFINITION
+		tags = {"@Smoketest"},//Tag from the feature file
+		plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, // report plug in 
+		monochrome = true
+		
+		)
+public class DressesRunner {
+
+
+}
+
